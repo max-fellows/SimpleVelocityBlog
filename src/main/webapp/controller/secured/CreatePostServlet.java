14612e1a1f4f1af5c4e79f35546ed1fa9486f49a
Max Fellows <max.fellows@gmail.com>
2017-05-15 21:58:28 -0700

    Initial commit.
    Plain Maven-based webapp using servlets, Velocity, Liquibase: proof of
    concept.

---
+++ src/main/webapp/controller/secured/CreatePostServlet.java
@@ -0,0 +1,25 @@
+package main.webapp.controller.secured;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CreatePost")
+@ServletSecurity(@HttpConstraint(rolesAllowed = "admin"))
+public class CreatePostServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/createpost.vm").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
