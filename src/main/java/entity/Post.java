e30b0fc302201e0368069b12896b5a26ea0fd898
Max Fellows <max.fellows@gmail.com>
2017-05-29 10:30:59 -0700

    Extremely basic bean usage in example page.

---
+++ src/main/java/entity/Post.java
@@ -0,0 +1,33 @@
+package main.java.entity;
+
+public class Post {
+
+	private int id = -1;
+	private String title = "new post";
+	private String contents = "new post";
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+}
--- src/main/webapp/controller/secured/CreatePostServlet.java
+++ src/main/webapp/controller/secured/CreatePostServlet.java
@@ -1,6 +1,7 @@
 package main.webapp.controller.secured;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -11,9 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+
+import main.java.entity.Post;
 
 @WebServlet("/post")
 @ServletSecurity(@HttpConstraint(rolesAllowed = "admin"))
@@ -24,7 +28,8 @@ public class CreatePostServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		VelocityContext ctx = new VelocityContext();
-		ctx.put("foo", "wheeeeeeeeeee");
+		Post post = new Post();
+		ctx.put("post", post);
 		try {
 			Template t = velocity.getTemplate("/post/new.vm");
 			t.merge(ctx, response.getWriter());
@@ -34,8 +39,19 @@ public class CreatePostServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Post post = new Post();
+		try {
+			BeanUtils.populate(post, request.getParameterMap());
+		} catch (IllegalAccessException e1) {
+			e1.printStackTrace();
+		} catch (InvocationTargetException e1) {
+			e1.printStackTrace();
+		}
+		
+		post.setId(1000);
+		
 		VelocityContext ctx = new VelocityContext();
-		ctx.put("foo", "wheeeeeeeeeee");
+		ctx.put("post", post);
 		try {
 			Template t = velocity.getTemplate("/post/new.vm");
 			t.merge(ctx, response.getWriter());
